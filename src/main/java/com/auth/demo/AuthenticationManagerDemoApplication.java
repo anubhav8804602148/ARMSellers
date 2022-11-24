4615b5f81a1a19722f6bcd064db742432cfbf6c6
Anubhav Sharma <Anubhav.Sharma@BRILLIO.COM>
2022-11-24 09:41:09 +0530

    Spring Boot basics
    	Single Controller only to greet.
    	Spring security already in dependency

---
+++ src/main/java/com/auth/demo/AuthenticationManagerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.auth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationManagerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationManagerDemoApplication.class, args);
+	}
+
+}
